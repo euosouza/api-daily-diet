c7024385f60e886b8ddee4bfa2e1a028ae3420cd
Victor Souza <euosouza@gmail.com>
2026-05-31 13:39:09 -0300

    chore: adiciona e implementa o @fastify/cookie para lidar com os cookies

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,12 @@
+import cookie from "@fastify/cookie";
 import fastify from "fastify";
-import { knex } from "./database";
 import { env } from "./env";
 
 const app = fastify();
 
 const PORT = env.PORT;
 
-app.get("/", async () => {
-  const migrations = await knex("knex_migrations").select("*");
-  return { migrations };
-});
+app.register(cookie);
 
 app
   .listen({
